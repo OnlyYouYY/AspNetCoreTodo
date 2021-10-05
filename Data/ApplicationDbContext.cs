214401d42fb7de471ab9a97aa1e478b60e5ca610
OnlyYouYY <gtr010yam@gmail.com>
2021-10-04 21:21:21 -0400

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+
+namespace AspNetCoreTodo.Data
+{
+    public class ApplicationDbContext 
+            : IdentityDbContext
+{
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<TodoItem> Items { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // ...
+    }
+}
+}
